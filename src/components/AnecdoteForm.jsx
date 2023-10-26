bab94cdf56addd76584bdd9b351bfc855499c337
m-lind <mari.j.lindstedt@gmail.com>
2023-10-26 11:47:28 +0300

    Used React Query for adding new anecdotes

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,21 +1,31 @@
-const AnecdoteForm = () => {
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createAnecdote } from "./../requests";
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+    },
+  });
+
+  const onCreate = event => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    newAnecdoteMutation.mutate({ content });
+  };
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
